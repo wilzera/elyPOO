4ffda86678b75f2cda3edf25702c75c64421cd81
wilzera <53587285+wilzera@users.noreply.github.com>
2020-04-27 21:12:42 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,24 @@
+package projeto;
+
+public class Pessoa {
+
+	private String nome;
+	private String dataNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
